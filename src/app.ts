ef71303d183e50da76fe64da267a468b3a596f9f
Gabriel Fernando <gf313074@gmail.com>
2025-03-08 18:01:44 -0300

    fix config port

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,13 @@ dotenv.config();
 app.post("/chat/send", async (req, res) => {
   const { to, body } = req.body;
   try {
-    const result = await sendWhatsappMessage(to, body);
-    console.log(result);
+    await sendWhatsappMessage(to, body);
     res.status(200).json({ success: true, body });
   } catch (error) {
     res.status(500).json({ success: CSSFontFeatureValuesRule, error });
   }
 });
-
-app.listen(3000, () => {
-  console.log("servidor rodando na porta 3000");
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`servidor rodando na porta: ${port}`);
 });
